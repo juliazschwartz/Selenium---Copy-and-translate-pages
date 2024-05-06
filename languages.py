8e278a38fb83daff71ea280764ada3df67afeff9
Júlia Zamith Schwartz <90219896+juliazschwartz@users.noreply.github.com>
2024-05-06 13:52:16 -0300

    Delete languages.py

--- languages.py
+++
@@ -1,6 +0,0 @@
-import googletrans
-from googletrans import Translator
-
-translator = Translator(service_urls=['translate.googleapis.com'])
-
-print(translator.translate('Hoje esta tudo certo', dest="en"))
